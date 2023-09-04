e8478099a61537018a5e21aba7f15c54f7439c9a
velayutham19931208 <138760043+velayutham19931208@users.noreply.github.com>
2023-09-04 20:36:40 +0530

    Update ec2creation.tf

--- ec2creation.tf
+++ ec2creation.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "foo" {
   ami           = "ami-002829755fa238bfa" # us-east-1
   instance_type = "t2.micro"
   tags = {
-      Name = "TF-Instance"
+      Name = "ec2-TF-Instance"
   }
 }
