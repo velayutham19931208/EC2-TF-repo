6343e5dfded16bcf64194e8c631aad3db33c6fdf
velayutham19931208 <138760043+velayutham19931208@users.noreply.github.com>
2023-09-22 19:08:06 +0530

    Update ec2creation.tf

--- ec2creation.tf
+++ ec2creation.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "foo" {
   ami           = "ami-002829755fa238bfa" # us-east-1
   instance_type = "t2.micro"
+  key_name      = "autojen_inst_in_ec2"
   tags = {
       Name = "ec2-TF-Instance"
   }
